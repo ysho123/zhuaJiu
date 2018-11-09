5267d3a5eade8dab9e1d25e3e377a9af0158d8a3
ysho123 <569513303@qq.com>
2018-11-09 18:18:28 +0800

    12
    1

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,11 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    buttonType: '',
+    inputContent : '今天谁下楼拿外卖',
+    joinNum : 0,
+    chooseNum : 0
   },
 
   onLoad: function() {
@@ -17,7 +16,9 @@ Page({
       console.log(openId);
       //判断此用户有没有在数据库留下记录
       wxUtils.hasUserInfo(()=>{
-        console.log('显示授权界面');
+        this.setData({
+          buttonType: 'getUserInfo'
+        });
       }
       );
     },
@@ -26,97 +27,55 @@ Page({
     })
   },
 
-  getUserInfo(res){
+  getUserInfo(res) {
     console.log(res);
-    if (res.detail.userInfo){
+    if (res.detail.userInfo) {
       let userInfo = res.detail.userInfo;
-      console.log('qqqqq',userInfo);
-      request.pullRequest('saveUser', userInfo)
-        .then(res=>{
-          console.log(res)
-        })
-    }
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
+      
+      wxUtils.request('userLogin', userInfo,(res)=>{
+        if(res.result){
+          this.setData({
+            buttonType : ''
+          });
+          wx.setStorageSync('userInfo', {hasLog : true});
+        }
       })
     }
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
+  submit(){
+    if (!this.data.buttonType){
+      console.log('222222222222');
+    }
+  },
+
+  inputNum(e){
+    let Type = e.currentTarget.dataset.id;
+    let value = e.detail.value;
+    let res = parseInt(value);
+    let modify = (Type == 'join' ? 'joinNum' : 'chooseNum');
+    this.setData({
+      [modify] : res
     })
   },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
+  minusNum(e){
+    let minusType = e.currentTarget.dataset.id;
+    let modify = (minusType == 'join' ? 'joinNum' : 'chooseNum');
+    let num = parseInt(this.data[modify]);
+    if (num == 0) return;
+    this.setData({
+      [modify]: num - 1
+    });
+  },
 
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
+  addNum(e){
+    let addType = e.currentTarget.dataset.id;
+    let modify = (addType == 'join' ? 'joinNum' : 'chooseNum');
+    let num = parseInt(this.data[modify]);
+    this.setData({
+      [modify]: num + 1
+    });
   },
 
 })
